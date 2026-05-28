1151f7064a8b3df30515da8a547d1457e1bde004
Salim YOUNSI <salim.ounsi@gmail.com>
2026-05-28 18:53:32 +0000

    fix rebase issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
 const path = require('path');
 
 module.exports = {
-  entry: './assets/js/script.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/js'),
-  },
-=======
-const path = require('path');
-
-module.exports = {
-  entry: './assets/js/script.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/js'),
-  },
->>>>>>> 0acfa95 (First initialisation)
+	  entry: './assets/js/script.js',
+	  output: {
+		      filename: 'bundle.js',
+		      path: path.resolve(__dirname, 'dist/js'),
+		    },
 };
+
