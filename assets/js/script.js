d1b068a98280695c36489f1683136275dd10c4c8
Salim YOUNSI <salim.ounsi@gmail.com>
2026-05-28 19:51:17 +0000

    fix rebase issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ import { initAnimations } from './animations.js';
 import { initContactForm } from './contact.js';
 import { initProjectModal } from './modal.js';
 import { initUI } from './ui.js';
-import { initThemeToggle } from './theme.js';
+import { initThemeToggle } from '../../theme.js';
 import { initParticles } from './particles.js';
 
 document.addEventListener('DOMContentLoaded', () => {
